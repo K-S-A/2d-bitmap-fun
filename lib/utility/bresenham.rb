3700d768241485484744741f2bae86b580665281
Sergey Konev <sergey@cimon.io>
2018-08-29 00:48:31 +0300

    Make line drawing algo more readable

--- lib/utility/bresenham.rb
+++ lib/utility/bresenham.rb
@@ -8,36 +8,88 @@ module Utility
   # Does not iterate whole canvas.
   module Bresenham
     def points(x1, y1, x2, y2)
-      if x1 > x2
-        points(x2, y2, x1, y1)
-      elsif x1 == x2 && y1 != y2 || (y2 - y1).abs > (x2 - x1)
-        points(y1, x1, y2, x2).map { |y, x| [x, y] }
-      else
-        approx_points(x1, y1, x2, y2)
-      end
+      Line.new(x1, y1, x2, y2).points
     end
 
-    private
+    class Line
+      attr_reader :x1,
+                  :y1,
+                  :x2,
+                  :y2,
+                  :vertical
 
-    def approx_points(x1, y1, x2, y2)
-      delta_x = x2 - x1
-      delta_y = (y2 - y1).abs
-      slope = y1 > y2 ? -1 : 1
+      def initialize(x1, y1, x2, y2)
+        @x1 = x1
+        @y1 = y1
+        @x2 = x2
+        @y2 = y2
 
-      y_error = 2 * delta_y
-      xy_error = y_error - (2 * delta_x)
-      delta = y_error - delta_x
-      y = y1
+        @vertical = vertical?
 
-      (x1..x2).each_with_object([]) do |x, arr|
-        arr << [x, y]
+        if vertical
+          @x1, @y1 = @y1, @x1
+          @x2, @y2 = @y2, @x2
+        end
 
+        return if @x1 < @x2
+
+        @x1, @x2 = @x2, @x1
+        @y1, @y2 = @y2, @y1
+      end
+
+      def points
+        @points ||= if vertical
+                      approx_points.map(&:reverse)
+                    else
+                      approx_points
+                    end
+      end
+
+      private
+
+      def vertical?
+        (y2 - y1).abs > (x2 - x1).abs
+      end
+
+      def approx_points
+        delta = y_derrivative - delta_x
+        y = y1
+
+        (x1..x2).each_with_object([]) do |x, arr|
+          arr << [x, y]
+          delta, y = next_delta_y(delta, y)
+        end
+      end
+
+      def next_delta_y(delta, y)
         if delta.positive?
-          delta += xy_error
+          delta += xy_derrivative
           y += slope
         else
-          delta += y_error
+          delta += y_derrivative
         end
+
+        [delta, y]
+      end
+
+      def delta_x
+        @delta_x ||= x2 - x1
+      end
+
+      def delta_y
+        @delta_y ||= (y2 - y1).abs
+      end
+
+      def slope
+        @slope ||= y1 > y2 ? -1 : 1
+      end
+
+      def y_derrivative
+        @y_derrivative ||= 2 * delta_y
+      end
+
+      def xy_derrivative
+        @xy_derrivative ||= y_derrivative - (2 * delta_x)
       end
     end
   end
