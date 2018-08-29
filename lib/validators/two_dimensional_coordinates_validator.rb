4523b4cf4b23566e893c6a50cce3a4e8ee1e23cf
Sergey Konev <sergey@cimon.io>
2018-08-29 14:01:57 +0300

    Fix variables naming

--- lib/validators/two_dimensional_coordinates_validator.rb
+++ lib/validators/two_dimensional_coordinates_validator.rb
@@ -22,24 +22,24 @@ module Validators
 
     private
 
-    def invalid_x?(x)
-      !x.is_a?(Integer) || x.negative? || x >= width
+    def invalid_x?(value)
+      !value.is_a?(Integer) || value.negative? || value >= width
     end
 
-    def invalid_y?(y)
-      !y.is_a?(Integer) || y.negative? || y >= height
+    def invalid_y?(value)
+      !value.is_a?(Integer) || value.negative? || value >= height
     end
 
     def invalid_size?(value)
       !value.is_a?(Integer) || !value.positive?
     end
 
-    def invalid_x_message(x)
-      "Invalid x coordinate: #{x}. Expected to be within 0..#{width.pred}"
+    def invalid_x_message(value)
+      "Invalid x coordinate: #{value}. Expected to be within 0..#{width.pred}"
     end
 
-    def invalid_y_message(y)
-      "Invalid y coordinate: #{y}. Expected to be within 0..#{height.pred}"
+    def invalid_y_message(value)
+      "Invalid y coordinate: #{value}. Expected to be within 0..#{height.pred}"
     end
 
     def invalid_size_message(value)
