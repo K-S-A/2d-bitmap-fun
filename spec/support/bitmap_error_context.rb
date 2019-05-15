010f5de4f1e74fd2914879c0e968701d64c1256c
Sergey Konev <sergey@cimon.io>
2019-05-15 08:11:29 +0300

    Fix rubocop warnings

--- spec/support/bitmap_error_context.rb
+++ spec/support/bitmap_error_context.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.shared_context 'coordinate error context', :coordinate_error_context do
+RSpec.shared_context 'with coordinate error context', :coordinate_error_context do
   let(:error_message) do
     /Invalid [xy]{1} coordinate: .*\. Expected to be within 0\.\.\d+/
   end
 end
 
-RSpec.shared_context 'color error context', :color_error_context do
+RSpec.shared_context 'with color error context', :color_error_context do
   let(:error_message) do
     /Invalid color value: .*\. Use one of:/
   end
